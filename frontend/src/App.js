65e74b037d5fe3d5e93472685d7f4124f7e172b3
sidharth-71 <sidharth.dhiman19@stu.srmuniversity.ac.in>
2022-12-03 12:36:50 +0530

    Initialised our frontend and backend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import { Heading } from '@chakra-ui/react';
+import './App.css';
+
+function App() {
+  return (
+  
+    <div className="App">
+     HIi
+    </div>
+  );
+}
+
+export default App;
